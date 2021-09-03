03e8cbc721960f6bbde8469856035956e40edf64
mjh-sakh <madjahead@gmail.com>
2021-09-03 15:16:03 +1100

    add env type show to ensure ENV vars working

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from task_manager.settings import DEBUG
 
 
 def index(request):
     return render(request, 'index.html', context={
         'who': 'World',
+        'env_type': 'dev' if DEBUG else 'prod',
     })
