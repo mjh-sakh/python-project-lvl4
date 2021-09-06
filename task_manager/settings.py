29873cf5119d929135dc59ed12601b9bbe31ce1f
mjh-sakh <madjahead@gmail.com>
2021-09-06 21:36:12 +1100

    add translation placeholders

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -116,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru' #'en-us'
 
 TIME_ZONE = 'Asia/Sakhalin'
 
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from task_manager.settings import DEBUG
+from django.utils.translation import gettext as _
 
 
 def index(request):
     return render(request, 'index.html', context={
-        'who': 'World',
+        'who': _('World'),
         'env_type': 'dev' if DEBUG else 'prod',
     })
