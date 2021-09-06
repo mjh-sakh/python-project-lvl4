550c60a613597ee6d8670169b7f206bb8445f3bf
mjh-sakh <madjahead@gmail.com>
2021-09-06 21:40:02 +1100

    fix delete old variable reference

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -20,7 +20,6 @@ env = environ.Env(
     DEBUG=(bool, False)
 )
 environ.Env.read_env()
-MY_VAR: str = env('MY_VAR')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
