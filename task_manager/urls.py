eff0ea296feb0f62afa00303a60363efdf9820fb
MjH.Sakh <48555070+mjh-sakh@users.noreply.github.com>
2021-09-16 19:36:33 +1100

    format

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from task_manager import views
-from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.index, name='home'),
