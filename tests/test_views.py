d925a379ffdc84712dcaa7c663ef8aa6cb6fff5e
mjh-sakh <madjahead@gmail.com>
2022-01-07 21:36:34 +1100

    complete test for /users/

--- tests/test_views.py
+++ tests/test_views.py
@@ -7,7 +7,11 @@ from pytest_django.asserts import assertTemplateUsed
 from task_manager import views
 
 
-@pytest.fixture(scope='session')
+def get_response_messages(response):
+    return list(response.context['messages'])
+
+
+@pytest.fixture(scope='function')
 def client():
     return Client()
 
@@ -20,6 +24,45 @@ def test_homepage_status_code_and_name_and_template(rf, client):
     assert response.status_code == 200
     assertTemplateUsed(response, 'index.html')
 
+
+@pytest.mark.django_db
+def test_users_page_status_code_and_name_and_template(client):
+    response = client.get('/users/', follow=True)
+    assert response.status_code == 200
+
+    response = client.get(reverse('users_list'))
+    assert response.status_code == 200
+    assertTemplateUsed(response, 'users_list.html')
+
+
+@pytest.mark.django_db
+def test_login_page_status_code_and_name_and_template_and_redirection_and_wrong_user_message_and_success_message(rf, client):
+    user_credentials = {
+        'username': 'test_user',
+        'password': 'test_pass',
+    }
+    wrong_credentials = {
+        'username': 'wrong_user',
+        'password': 'wrong_pass',
+    }
+
+    response = client.get('/login/', follow=True)
+    assert response.status_code == 200
+
+    response = client.get(reverse('login'))
+    assert response.status_code == 200
+    assertTemplateUsed(response, 'login.html')
+
+    user = User.objects.create_user(**user_credentials)
+    response = client.post('/login/', data=wrong_credentials, follow=True)
+    assertContains(response,
+                   'Пожалуйста, введите правильные имя пользователя и пароль')
+
+    response = client.post('/login/', data=user_credentials, follow=True)
+    assertTemplateUsed(response, 'index.html')
+    messages = get_response_messages(response)
+    assert messages[0].tags == 'success'
+    assert messages[0].message == 'Рады вас видеть снова'
 
 
 # def test_test(rf):
